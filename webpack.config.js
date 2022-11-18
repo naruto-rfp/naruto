577a5e7c4830c393f0108451985ffcf1c15ed7f5
Kee Wook Lee <keewook2@gmail.com>
2022-11-18 10:59:14 +0900

    setup babel and webpack config

---
+++ webpack.config.js
@@ -0,0 +1,82 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const sass = require('sass');
+
+module.exports = (env, argv) => {
+  const prod = argv.mode === 'production';
+
+  return {
+    mode: prod ? 'production' : 'development',
+    devtool: prod ? 'hidden-source-map' : 'eval',
+    entry: path.join(__dirname, 'client/src/index.js'),
+    output: {
+      publicPath: '/',
+      path: path.join(__dirname, 'client/dist'),
+      filename: 'bundle.[hash].js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s?css$/,
+          use: [
+            {
+              loader: 'style-loader',
+              options: {
+                injectType: 'singletonStyleTag',
+              },
+            },
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                implementation: sass,
+              }
+            }
+          ],
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+          }
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['.js','.jsx'],
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        React: 'react',
+      }),
+      new HtmlWebpackPlugin({
+        template: './client/public/index.html',
+        minify: process.env.NODE_ENV === 'production' ? {
+          collapseWhitespace: true,
+          removeComments: true,
+        } : false,
+      }),
+      new CleanWebpackPlugin(),
+    ],
+    devServer: {
+      open: true,
+      host: 'localhost',
+      historyApiFallback: true,
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000',
+          secure: false,
+        },
+      },
+    },
+  }
+}
