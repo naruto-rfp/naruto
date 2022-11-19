ac979ad37f22a25de6989d1f8e75eeeddd659069
Connor Fung <cmfung23@gmail.com>
2022-11-19 10:46:13 -0800

    to set up server side file structure and start on database

---
+++ server/database/schema.sql
@@ -0,0 +1,22 @@
+/* drop database and tables if exists */
+
+/* create database */
+
+
+
+
+CREATE TABLE IF NOT EXISTS user {
+  user_id SERIAL PRIMARY KEY,
+  user_firstname VARCHAR(60) NOT NULL,
+
+}
+
+
+
+
+
+
+
+
+
+/* eCommerce Table */
