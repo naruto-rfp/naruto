9143285d9348966a79283621eb249e8af3b4ef99
Connor Fung <cmfung23@gmail.com>
2022-11-21 22:20:08 -0800

    to center modal

--- client/src/pages/Login/index.jsx
+++ client/src/pages/Login/index.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Login() {
   return (
-    <section className="h-full gradient-form bg-gray-200 md:h-screen">
-      <div className="container py-6 px-6 h-full justify-center items-center">
+    <section className="h-full gradient-form bg-gray-200 md:h-screen justify-center">
+      <div className="container py-6 px-6 h-full justify-center items-center mx-auto">
         <div className="flex justify-center items-center flex-wrap h-full g-6 text-gray-800">
           <div className="xl:w-10/12">
             <div className="block bg-white shadow-lg rounded-lg">
