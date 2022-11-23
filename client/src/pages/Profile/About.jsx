1ca23233bbbf2fbc084836827388b4d93faa4210
Kee Wook Lee <keewook2@gmail.com>
2022-11-23 19:23:15 +0900

    add skeleton for About, Info part on Profile page

---
+++ client/src/pages/Profile/About.jsx
@@ -0,0 +1,10 @@
+export default function Abount({ about }) {
+  return (
+    <div>
+      about is : {about}
+      <button className="bg-artichoke text-white font-bold py-2 px-4 rounded" type="button">
+        Edit Profile
+      </button>
+    </div>
+  )
+}
--- client/src/pages/Profile/index.jsx
+++ client/src/pages/Profile/index.jsx
@@ -1,6 +1,47 @@
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
 import RadarChart from './RadarChart'
+import Info from './Info'
+import About from './About'
 
 export default function Profile() {
+  const { id } = useParams()
+  const [userData, setUserData] = useState({
+    id: null,
+    firstName: '',
+    lastName: '',
+    about: '',
+    email: '',
+    profilePic: '',
+    speed: 0,
+    reliability: 0,
+    strength: 0,
+    jumping: 0,
+    aerobic: 0,
+  })
+
+  useEffect(() => {
+    // TODO: replace 1 to { id } in useParams
+    axios.get('/api/user/1').then((results) => {
+      console.log(results)
+      setUserData((prev) => ({
+        ...prev,
+        id,
+        firstName: results.data.firstName,
+        lastName: results.data.lastName,
+        about: results.data.about,
+        email: results.data.email,
+        profilePic: results.data.profilePic,
+        speed: results.data.speed,
+        reliability: results.data.reliability,
+        strength: results.data.strength,
+        jumping: results.data.aerobic,
+      }))
+    })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <section className="h-full justify-center">
       <div className="text-gray-600 body-font">
@@ -13,7 +54,11 @@ export default function Profile() {
         </div>
         <div className="grid grid-cols-4 gap-4">
           <div className="items-center text-center bg-darkBlueGray rounded-lg h-96">
-            Name, Info here
+            <Info
+              firstName={userData.firstName}
+              lastName={userData.lastName}
+              profilePic={userData.profilePic}
+            />
           </div>
           <div className="border-solid border-2 border-indigo-600 items-center text-center h-96">
             <RadarChart />
@@ -21,7 +66,9 @@ export default function Profile() {
           <div className="items-center text-center h-96">Followers</div>
           <div className="items-center text-center h-96">Members</div>
         </div>
-        <div className="flex justify-center items-center flex-wrap mt-10">About here.</div>
+        <div className="flex justify-center items-center flex-wrap mt-10">
+          <About about={userData.about} />
+        </div>
       </div>
     </section>
   )
