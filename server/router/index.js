58497d87149647f53fbdb703286be36643495350
Lawrence Good <lawrence.good55@gmail.com>
2022-11-23 13:34:46 -0800

    Update datasets, add controllers for teams page

--- server/router/index.js
+++ server/router/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const controllers = require('../controllers')
+const coaches = require('../controllers/coaches')
+const members = require('../controllers/members')
+const fans = require('../controllers/fans')
 const store = require('../controllers/products')
 
 const router = express.Router()
@@ -8,6 +11,9 @@ router.get('/', (_, res) => {
   res.send('Sample API route')
 })
 
+/**
+ * Users
+ */
 // Create User
 router.post('/user', controllers.createUser)
 // Get User Profile by ID
@@ -19,12 +25,19 @@ router.get('/session', controllers.getSession)
 router.post('/login', controllers.login)
 router.post('/logout', controllers.logout)
 
+/**
+ * Products
+ */
 // Retrieve Product information
 router.get('/products', store.getProducts)
 // Add Product to Database
 router.post('/products', store.postProducts)
 // Delete Product from Database
 router.delete('/products/:id', store.deleteProduct)
+
+/**
+ * SKUs
+ */
 // Retrieve SKU information
 router.get('/skus', store.getSkus)
 router.get('/skus/id', store.getSkuById)
@@ -32,5 +45,20 @@ router.get('/skus/id', store.getSkuById)
 router.post('/skus', store.postSkus)
 // Delete SKUs from Database
 router.delete('/skus/:id', store.deleteSkus)
+
+/**
+ * Coaches
+ */
+router.get('/coaches/:teamID', coaches.getCoaches)
+
+/**
+ * Members
+ */
+router.get('/members/:teamID', members.getMembers)
+
+/**
+ * Fans
+ */
+router.get('/fans/:teamID', fans.getFans)
 
 module.exports = router
