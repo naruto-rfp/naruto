bcad77c094136b0a3a427a5dec343c3953444141
Connor Fung <cmfung23@gmail.com>
2022-11-27 14:40:51 -0800

    to get logout working

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState, createContext } from 'react'
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom'
 import Home from './pages/Homepage'
 import Profile from './pages/Profile'
 import Store from './pages/Store'
@@ -9,28 +9,13 @@ import Checkout from './pages/Checkout'
 import PrivateRoutes from './components/PrivateRoutes'
 import { useStore } from './lib/fastContext'
 
-const UserContext = createContext()
+// const UserContext = createContext()
 
 const App = function App() {
-  const navigate = useNavigate()
+  // const navigate = useNavigate()
   const [currentUser, setCurrentUser] = useState('')
   const [session, setSession] = useStore('session')
-  const logout = () => {
-    fetch('/api/session', { method: 'DELETE', credentials: 'include' })
-      .then(() => console.log('logged out'))
-      .catch((err) => console.log(err))
-    // Redirect to the login page after logging out to force a refresh
-    // window.location.assign('/')
-    // If want to keep user on the app without hard refresh:
-    // ... use react-router-dom to navigate to the login or home page
-    //
-    // TEMP
-    console.log('Trying to log out')
-    navigate('/login')
-    setSession(null)
-  }
 
-  // Get the user session on intial mounting
   useEffect(() => {
     fetch('/api/session', { credentials: 'include' })
       .then((res) => res.json())
@@ -50,15 +35,19 @@ const App = function App() {
 
   return (
     <BrowserRouter>
+      <Outlet currentUser={currentUser} />
       <Routes>
-        <Route element={<PrivateRoutes session={session} logout={logout} />}>
+        <Route element={<PrivateRoutes session={session} setSession={setSession} />}>
           <Route element={<Home />} path="/" />
           <Route element={<Profile />} path="/profile/:id" />
           <Route element={<Store />} path="/store" />
           <Route element={<Team />} path="/team" />
           <Route element={<Checkout />} path="/checkout" />
         </Route>
-        <Route element={<Login setSession={setSession} />} path="/login" />
+        <Route
+          element={<Login setSession={setSession} setCurrentUser={setCurrentUser} />}
+          path="/login"
+        />
       </Routes>
     </BrowserRouter>
   )
