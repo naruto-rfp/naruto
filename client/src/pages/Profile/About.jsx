ea434e79e22a828bb23435603a8d532dc1b7e6cb
Kee Wook Lee <keewook2@gmail.com>
2022-11-27 21:26:39 +0900

    added about and modal for editing profile

--- client/src/pages/Profile/About.jsx
+++ client/src/pages/Profile/About.jsx
@@ -1,10 +1,53 @@
-export default function Abount({ about }) {
+import { useState } from 'react'
+import EditProfileModal from './EditProfileModal'
+
+export default function About({ userData, about }) {
+  const [modal, setModal] = useState(false)
+  const [inputs, setInputs] = useState({
+    speed: userData.speed,
+    reliability: userData.reliability,
+    jumping: userData.jumping,
+    aerobic: userData.aerobic,
+    strength: userData.strength,
+    about: userData.about,
+  })
+
+  const handleEditModalPopUp = () => {
+    setModal((prev) => !prev)
+  }
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target
+    setInputs({
+      ...inputs,
+      [name]: value,
+    })
+  }
+
+  const handleEditProfileSubmit = (e) => {
+    e.preventDefault()
+    console.log('form submitted!')
+    // axios post profile change
+  }
+
   return (
     <div>
       about is : {about}
-      <button className="bg-artichoke text-white font-bold py-2 px-4 rounded" type="button">
+      <button
+        className="bg-gradient-to-r from-blackCoral to-greenYellow text-white float-right font-bold py-2 px-4 rounded"
+        type="button"
+        onClick={handleEditModalPopUp}
+      >
         Edit Profile
       </button>
+      {modal ? (
+        <EditProfileModal
+          modal={modal}
+          handleEditModalPopUp={handleEditModalPopUp}
+          handleInputChange={handleInputChange}
+          handleEditProfileSubmit={handleEditProfileSubmit}
+        />
+      ) : null}
     </div>
   )
 }
