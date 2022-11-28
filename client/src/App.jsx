6402d4b8525c15d40883ab11754941cb29a6b20e
Kevin Zhang <kevz.workspace@gmail.com>
2022-11-27 20:53:49 -0800

    commit before merge

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -42,13 +42,11 @@ const App = function App() {
 
   return (
     <BrowserRouter>
-        <NavBar />
       <Routes>
         <Route element={<PrivateRoutes session={session} logout={logout} />}>
           <Route element={<Home />} path="/" />
           <Route element={<Profile />} path="/profile/:id" />
           <Route element={<Store />} path="/store" />
-          {/* <Route element={<Team />} path="/team" /> */}
           <Route element={<Team />} path="/team/:id" />
           <Route element={<Checkout />} path="/checkout" />
         </Route>
